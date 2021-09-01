92b374ba1a0c68062ff0ce3039566c39e6fc9669
Joel Kuhn <joelakuhn@gmail.com>
2021-08-31 23:07:05 -0400

    Added free method for strings
    Unfortunately I can't use it yet because Dart is giving me this
    completely bonkers error message:
    
    The type 'Void Function(Pointer<Utf8>)' must be a subtype of 'Void Function(Pointer<Utf8>)' for 'asFunction'.

--- native/qr-reader-ffi/src/lib.rs
+++ native/qr-reader-ffi/src/lib.rs
@@ -13,3 +13,13 @@ pub extern "C" fn read_qr_ffi(path_ptr: *const c_char, max_size_ptr : *const c_c
     let qr_data = read_qr(path, max_size);
     return CString::new(qr_data).unwrap().into_raw();
 }
+
+#[no_mangle]
+pub extern "C" fn free_str_ffi(ptr: *mut c_char) {
+    unsafe {
+        if ptr.is_null() {
+            return;
+        }
+        CString::from_raw(ptr);
+    }
+}
