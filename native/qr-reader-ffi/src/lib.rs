e1a37736f06c37c3d355a4c40888cab81c1f5e72
Joel Kuhn <joelakuhn@gmail.com>
2021-09-02 22:56:08 -0400

    Added panic catch to read_qr_ffi to avoid crashes

--- native/qr-reader-ffi/src/lib.rs
+++ native/qr-reader-ffi/src/lib.rs
@@ -4,14 +4,21 @@ use std::os::raw::c_char;
 
 #[no_mangle]
 pub extern "C" fn read_qr_ffi(path_ptr: *const c_char, max_size_ptr : *const c_char) -> *const c_char {
-    let path_cstr = unsafe { CStr::from_ptr(path_ptr) };
-    let path = String::from(path_cstr.to_str().unwrap());
+    let res = std::panic::catch_unwind(|| {
+        let path_cstr = unsafe { CStr::from_ptr(path_ptr) };
+        let path = String::from(path_cstr.to_str().unwrap());
 
-    let max_size_cstr = unsafe { CStr::from_ptr(max_size_ptr) };
-    let max_size = String::from(max_size_cstr.to_str().unwrap()).parse().unwrap();
+        let max_size_cstr = unsafe { CStr::from_ptr(max_size_ptr) };
+        let max_size = String::from(max_size_cstr.to_str().unwrap()).parse().unwrap();
 
-    let qr_data = read_qr(path, max_size);
-    return CString::new(qr_data).unwrap().into_raw();
+        let qr_data = read_qr(path, max_size);
+        return CString::new(qr_data).unwrap().into_raw();
+    });
+
+    return match res {
+        Ok(qr_data) => qr_data,
+        _ => CString::new("").unwrap().into_raw()
+    };
 }
 
 #[no_mangle]
