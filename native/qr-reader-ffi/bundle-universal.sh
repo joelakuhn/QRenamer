831e10379f2f08a9c0f2ac161b65edd722b9c89b
Joel Kuhn <joelakuhn@gmail.com>
2023-08-29 23:39:17 -0400

    Change the bundle universal script to also build the targets

--- native/qr-reader-ffi/bundle-universal.sh
+++ native/qr-reader-ffi/bundle-universal.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+cargo build --release && \
+cargo build --target=aarch64-apple-darwin --release && \
 lipo -create -arch arm64 target/aarch64-apple-darwin/release/libqr_reader_ffi.dylib -arch x86_64 target/release/libqr_reader_ffi.dylib -output libqr_reader_ffi.dylib
+
 file libqr_reader_ffi.dylib
+
