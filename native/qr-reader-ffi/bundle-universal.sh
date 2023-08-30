5448a6d5d5fd8aee47347c61fe280c5f3c635924
Joel Kuhn <joelakuhn@gmail.com>
2023-08-29 21:57:59 -0400

    Change ffi library to a universal dylib
    Rust doesn't have a built-in way to do this, so I'm just calling lipo
    through a script.

---
+++ native/qr-reader-ffi/bundle-universal.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+lipo -create -arch arm64 target/aarch64-apple-darwin/release/libqr_reader_ffi.dylib -arch x86_64 target/release/libqr_reader_ffi.dylib -output libqr_reader_ffi.dylib
+file libqr_reader_ffi.dylib
