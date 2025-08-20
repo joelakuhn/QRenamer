e05652aacadf2af29cbf906c752d3ba929c19e0f
Joel <joelakuhn@gmail.com>
2025-08-19 20:56:10 -0400

    Update windows build

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -29,7 +29,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(&run_loop, project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(720, 550);
+  Win32Window::Size size(1024, 720);
   if (!window.CreateAndShow(L"QRenamer", origin, size)) {
     return EXIT_FAILURE;
   }
